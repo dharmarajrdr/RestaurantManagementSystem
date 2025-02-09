9e7e43f91a3e23847a593db7a859a795993e6485
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-02-09 15:17:20 +0530

    Added custom exception

---
+++ src/main/java/com/dharmaraj/restaurant_management_system/custom_exception/NoStrategyFound.java
@@ -0,0 +1,9 @@
+package com.dharmaraj.restaurant_management_system.custom_exception;
+
+
+public class NoStrategyFound extends Exception {
+
+    public NoStrategyFound(String message) {
+        super(message);
+    }
+}
