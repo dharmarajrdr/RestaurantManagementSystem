d938f39f047435b3b4bfdc9fe9f1d7a9b2ad3940
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-02-09 14:39:51 +0530

    Added controller

--- src/main/java/com/dharmaraj/restaurant_management_system/controllers/MenuController.java
+++ src/main/java/com/dharmaraj/restaurant_management_system/controllers/MenuController.java
@@ -2,6 +2,10 @@ package com.dharmaraj.restaurant_management_system.controllers;
 
 import java.util.List;
 
+import com.dharmaraj.restaurant_management_system.custom_exception.UnauthorizedAccessException;
+import com.dharmaraj.restaurant_management_system.custom_exception.UserNotFoundException;
+import com.dharmaraj.restaurant_management_system.dtos.AddMenuItemRequestDto;
+import com.dharmaraj.restaurant_management_system.dtos.AddMenuItemResponseDto;
 import com.dharmaraj.restaurant_management_system.dtos.GetMenuItemsRequestDto;
 import com.dharmaraj.restaurant_management_system.dtos.GetMenuItemsResponseDto;
 import com.dharmaraj.restaurant_management_system.dtos.ResponseStatus;
@@ -14,6 +18,24 @@ public class MenuController {
 
     public MenuController(MenuService menuService) {
         this.menuService = menuService;
+    }
+
+    public AddMenuItemResponseDto addMenuItem(AddMenuItemRequestDto requestDto) {
+        AddMenuItemResponseDto addMenuItemResponseDto = new AddMenuItemResponseDto();
+        try {
+            long userId = requestDto.getUserId();
+            String name = requestDto.getName();
+            double price = requestDto.getPrice();
+            String dietaryRequirement = requestDto.getDietaryRequirement();
+            String itemType = requestDto.getItemType();
+            String description = requestDto.getDescription();
+            MenuItem menuItem = menuService.addMenuItem(userId, name, price, dietaryRequirement, itemType, description);
+            addMenuItemResponseDto.setMenuItem(menuItem);
+            addMenuItemResponseDto.setStatus(ResponseStatus.SUCCESS);
+        } catch (UserNotFoundException | UnauthorizedAccessException e) {
+            addMenuItemResponseDto.setStatus(ResponseStatus.FAILURE);
+        }
+        return addMenuItemResponseDto;
     }
 
     public GetMenuItemsResponseDto getMenuItems(GetMenuItemsRequestDto requestDto) {
