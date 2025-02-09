a8a3bd0f1b2dc61edcf3246270f6e84ed1e6fb1e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-02-09 13:01:51 +0530

    Added dtos

---
+++ src/main/java/com/dharmaraj/restaurant_management_system/dtos/GenerateBillRequestDto.java
@@ -0,0 +1,13 @@
+package com.dharmaraj.restaurant_management_system.dtos;
+
+public class GenerateBillRequestDto {
+    private long userId;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
